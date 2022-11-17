97e5116d33f5fca4859c2d10e0dfcf4d41789118
luismingati <luismingati@gmail.com>
2022-11-16 22:34:29 -0300

    ok!!!!

--- data.py
+++ data.py
@@ -1,12 +1,16 @@
 import pickle
 
 def saveData(obj):
-  with open("usuarioTeste.pickle", 'ab') as arquivo:
+  with open("usuarioTeste.pickle", 'wb') as arquivo:
       pickle.dump(obj, arquivo)
       arquivo.close()
     
 def loadData():
+  data = []
   with open("usuarioTeste.pickle", 'rb') as arquivo:
-    arquive = pickle.load(arquivo)
-    arquivo.close()
-    return arquive
+    try:
+        while True:
+            data.append(pickle.load(arquivo))
+    except EOFError:
+        pass
+    return data
--- projeto1.py
+++ projeto1.py
@@ -1,24 +1,24 @@
 from verRestaurantes import *
 
 
-# passeioMaragogi = Atividade( "imagem passeio Maragogi",  "Passeio à maragogi", [5, 4, 3], "Saindo de maceió, ao norte, você visitará piscinas naturais", "9h", ["Bom para crianças","Natureza","Praia","Pet friendly", "Bom para idosos"], ["Catamarã", "Apoio no Restaurante pontal do maragogi"], "Pontal do Maragogi, Rodovia AL 101 Norte, Km 130 s/n Burgalhau - Barra Grande, Maragogi - AL, 57799-000, Brazil", [], "passeio")
-# passeioMarape = Atividade( "imagem passeio Marapé",  "Passeio às Dunas de Marapé", [5, 4, 3], "Paraíso ecológico formado entre a Praia de Duas Barras e o Rio Jequiá. Além disso, pode também visualizar falésias.", "7h", ["Natureza","Aventura"], ["passeio de buggy", "Barraquinha","Day-use", "Circuito Pau-de-Arara", "Trilha dos Caetés"], "Povoado Barra de Jequia SN Duas Barras - Jequiá da Praia - Litoral Sul de Alagoas - 50 min de Maceió, Jequiá da Praia, Alagoas 57244-000 Brasil", [], "passeio")
-# passeioHibiscus = Atividade( "imagem passeio Hibiscus",  "Passeio à ipipoca no Hibiscus beach club", [5, 4, 3], "Ida a praia de IPIOCA pra aproveitar um dia relaxante no beach club.", "3h30", ["Relaxante","Para casais","Bom para crianças","Natureza","Praia"], ["Passeios Náuticos", "Massagem relaxante","Área HIBISQUINHO para crianças","Passeio de lancha, Stand-up paddling e Caiaque"], "Rodovia AL 101 Norte, Bairro Ipioca Residencial Angra de Ipioca, Maceió, Alagoas 57039-705 Brasil", [], "passeio")
+passeioMaragogi = Atividade( "imagem passeio Maragogi",  "Passeio à maragogi", [5, 4, 3], "Saindo de maceió, ao norte, você visitará piscinas naturais", "9h", ["Bom para crianças","Natureza","Praia","Pet friendly", "Bom para idosos"], ["Catamarã", "Apoio no Restaurante pontal do maragogi"], "Pontal do Maragogi, Rodovia AL 101 Norte, Km 130 s/n Burgalhau - Barra Grande, Maragogi - AL, 57799-000, Brazil", [], "passeio")
+passeioMarape = Atividade( "imagem passeio Marapé",  "Passeio às Dunas de Marapé", [5, 4, 3], "Paraíso ecológico formado entre a Praia de Duas Barras e o Rio Jequiá. Além disso, pode também visualizar falésias.", "7h", ["Natureza","Aventura"], ["passeio de buggy", "Barraquinha","Day-use", "Circuito Pau-de-Arara", "Trilha dos Caetés"], "Povoado Barra de Jequia SN Duas Barras - Jequiá da Praia - Litoral Sul de Alagoas - 50 min de Maceió, Jequiá da Praia, Alagoas 57244-000 Brasil", [], "passeio")
+passeioHibiscus = Atividade( "imagem passeio Hibiscus",  "Passeio à ipipoca no Hibiscus beach club", [5, 4, 3], "Ida a praia de IPIOCA pra aproveitar um dia relaxante no beach club.", "3h30", ["Relaxante","Para casais","Bom para crianças","Natureza","Praia"], ["Passeios Náuticos", "Massagem relaxante","Área HIBISQUINHO para crianças","Passeio de lancha, Stand-up paddling e Caiaque"], "Rodovia AL 101 Norte, Bairro Ipioca Residencial Angra de Ipioca, Maceió, Alagoas 57039-705 Brasil", [], "passeio")
 
-# arrayUsuarios = usuarios
-# arrayAtividades = [passeioMaragogi, passeioHibiscus, passeioMarape]
-# arrayPasseios = [passeioMaragogi, passeioMarape, passeioHibiscus]
-# arrayPraias = [passeioMarape, passeioHibiscus]
-# arrayRestaurantes = [passeioMaragogi, passeioMaragogi]
+arrayUsuarios = usuarios
+arrayAtividades = [passeioMaragogi, passeioHibiscus, passeioMarape]
+arrayPasseios = [passeioMaragogi, passeioMarape, passeioHibiscus]
+arrayPraias = [passeioMarape, passeioHibiscus]
+arrayRestaurantes = [passeioMaragogi, passeioMaragogi]
 
-# dictTudo = {
-#     "usuarios" : arrayUsuarios, 
-#     "atividades" : arrayAtividades,
-#     "passeios" : arrayPasseios,
-#     "praias" : arrayPraias,
-#     "restaurantes" : arrayRestaurantes,
-# }
-# saveData(dictTudo)
+dictTudo = {
+    "usuarios" : arrayUsuarios, 
+    "atividades" : arrayAtividades,
+    "passeios" : arrayPasseios,
+    "praias" : arrayPraias,
+    "restaurantes" : arrayRestaurantes,
+}
+saveData(dictTudo)
 
 archive =  loadData()
 
